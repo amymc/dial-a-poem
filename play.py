5c0eee0c998fb697c3b1c2afc164b06fc70f5d35
amymc <mccarthy.amy.n@gmail.com>
2024-04-13 18:49:45 -0400

    fix url

--- play.py
+++ play.py
@@ -3,34 +3,33 @@
 import vlc
 import random
 import time
-import RPi.GPIO as GPIO
+# import RPi.GPIO as GPIO
 
-track_names = ["vajra-kisses", "pressure", "cemetery-hill", "green-automobile"]
+track_names = ["vajra-kisses", "pressure", "cemetery-hill", "green-automobile", "mushroom_haiku"]
 
 def play_random_track():
     randTrack = random.choice(track_names)
     # print(randTrack)
-    print("https://github.com/amymc/push-a-poem/blob/master/tracks% s.mp3?raw=true" % (randTrack))
-    p = vlc.MediaPlayer(randTrack)
+    p = vlc.MediaPlayer("https://github.com/amymc/push-a-poem/blob/master/tracks/% s.mp3?raw=true" % (randTrack))
     p.play()
 
     while True:
         pass
 
-BUTTON_PIN = 16
-GPIO.setmode(GPIO.BCM)  
-GPIO.setup(BUTTON_PIN, GPIO.IN, pull_up_down=GPIO.PUD_UP)  
+# BUTTON_PIN = 16
+# GPIO.setmode(GPIO.BCM)  
+# GPIO.setup(BUTTON_PIN, GPIO.IN, pull_up_down=GPIO.PUD_UP)  
 
-try:
-    while True:
-        time.sleep(0.1)
-        if (GPIO.input(BUTTON_PIN) == GPIO.LOW) and (button_pressed is False):
-            print("button pressed")
-            button_pressed = True
-            play_random_track()
-        elif (GPIO.input(BUTTON_PIN) == GPIO.HIGH):
-            button_pressed = False
-except KeyboardInterrupt:
-    GPIO.cleanup()
+# try:
+#     while True:
+#         time.sleep(0.1)
+#         if (GPIO.input(BUTTON_PIN) == GPIO.LOW) and (button_pressed is False):
+#             print("button pressed")
+#             button_pressed = True
+#             play_random_track()
+#         elif (GPIO.input(BUTTON_PIN) == GPIO.HIGH):
+#             button_pressed = False
+# except KeyboardInterrupt:
+#     GPIO.cleanup()
 
-# play_random_track()
+play_random_track()
